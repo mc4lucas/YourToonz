14c5cd7e27e9459c0084f7cda38cc9ef3f81ab73
Lucas Ross <lucasross1234@gmail.com>
2021-01-14 14:11:30 -0500

    added sw files from barntracker

--- sw.js
+++ sw.js
@@ -1,20 +1,25 @@
-// On install - caching the application shell
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      // cache any static files that make up the application shell
-      return cache.add('index.html');
-    })
-  );
-});
+// // On install - caching the application shell
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open('sw-cache').then(function(cache) {
+//       // cache any static files that make up the application shell
+//       return cache.add('index.html');
+//     })
+//   );
+// });
 
-// On network request
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      //If response found return it, else fetch again
-      return response || fetch(event.request);
-    })
-  );
-});
+// // On network request
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     // Try the cache
+//     caches.match(event.request).then(function(response) {
+//       //If response found return it, else fetch again
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+"use strict";
+importScripts('sw-toolbox.js');
+toolbox.precache(["index.html","styles.css", "music.js"]);
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
